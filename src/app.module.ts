c324083c60887d5ce93e23155ff097162a164b92
xshyn <shayanahmadi2004@gmail.com>
2026-08-27 14:03:25 +0330

    feat: init structure & init users entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { CartsModule } from './carts/carts.module';
+import { InventoryModule } from './inventory/inventory.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule, OrdersModule, CartsModule, InventoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
